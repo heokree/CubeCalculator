a8d6827e5c686b077a7d33a49518bcd77ee14c67
HeokreeLim <heokree.lim@gmail.com>
2021-05-26 00:18:57 +0900

    Second Commit

---
+++ Assets/02.Scripts/CalMethods.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class CalMethods : MonoBehaviour
+{
+    public double Add(double num1, double num2)
+    {
+        return num1 + num2;
+    }
+
+    public double Subtract(double num1, double num2)
+    {
+        return num1 - num2;
+    }
+
+    public double Multiply(double num1, double num2)
+    {
+        return num1 * num2;
+    }
+
+    public double Divide(double num1, double num2)
+    {
+        return num1 / num2;
+    }
+
+}
